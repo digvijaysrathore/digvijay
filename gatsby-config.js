32761a11c941b5169f40d3b84d68f9dbb4246de2
Digvijay Singh Rathore <dynamicdigvijay@gmail.com>
2020-08-26 18:01:12 +0530

    reflex ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,32 @@ require(`dotenv`).config()
 
 module.exports = {
   siteMetadata: {
-    title: "Reflex",
-    description: "Starter for Reflex.",
+    title: "digvijay",
+    description: "Hi I'm Digvijay",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
-  plugins: ["@reflexjs/gatsby-theme-base"],
+  plugins: [
+    "@reflexjs/gatsby-theme-base",
+    {
+      resolve: `@reflexjs/gatsby-theme-post`,
+      options: {
+        contentPath: "content/posts",
+        basePath: "/blog",
+        postsPerPage: 4,
+        pageQuery: null,
+        slugResolver: null,
+      }
+    },
+    {
+      resolve: `@reflexjs/gatsby-theme-video`,
+      options: {
+        contentPath: "content/videos",
+        basePath: "/videos",
+        videosPerPage: 10,
+        pageQuery: null,
+        slugResolver: null,
+        providers: []
+      }
+    }
+  ],
 }
